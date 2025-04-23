328509bf3fc1354c27adb6ded5b3f75d04980fea
TasnimRiza <tasnimrahmanriza@gmail.com>
2025-04-23 15:39:47 +0600

    1st commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module sulthanatasnimriza.group_62_desco_2111329_sec_7_oop {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens sulthanatasnimriza.group_62_desco_2111329_sec_7_oop to javafx.fxml;
+    exports sulthanatasnimriza.group_62_desco_2111329_sec_7_oop;
+}
---
+++ src/main/java/sulthanatasnimriza/group_62_desco_2111329_sec_7_oop/HelloController.java
@@ -0,0 +1,14 @@
+package sulthanatasnimriza.group_62_desco_2111329_sec_7_oop;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
